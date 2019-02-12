e6b528513ba0b83330a33251a418418d88636fc0
Eric Johnson <eric.johnson@code42.com>
2019-02-12 16:27:18 -0600

    add nvm setup to conf

--- conf/bashrc.bash
+++ conf/bashrc.bash
@@ -15,6 +15,9 @@ export PATH=~/dev/osx-dev-env/bin:$PATH
 
 export NVM_DIR="$HOME/.nvm" 
 
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 ############
 # golang
 ############
